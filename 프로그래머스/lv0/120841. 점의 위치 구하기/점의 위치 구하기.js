3ea35e6aa534885bec19c9dcd81daaae533cc1d0
HYEMBURGERWORLD <123081489+HYEMBURGERWORLD@users.noreply.github.com>
2023-03-04 20:52:54 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,8 @@
+function solution(dot) {
+    const answer =
+          (dot[0] > 0 && dot[1] > 0) ? 1 :
+    (dot[0] > 0 && dot[1] < 0) ? 4 :
+    (dot[0] < 0 && dot[1] > 0) ? 2 : 3;
+    
+    return answer;
+}
