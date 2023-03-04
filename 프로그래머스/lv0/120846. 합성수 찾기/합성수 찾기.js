c0732b3d940ee0dadc95e38ed0536371b98cf411
HYEMBURGERWORLD <123081489+HYEMBURGERWORLD@users.noreply.github.com>
2023-03-04 20:58:40 +0900

    [level 0] Title: 합성수 찾기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let answer = new Set();
+    for(let i = 1; i <= n; i++) {
+        for(let j = 2; j < i; j++) {
+            if(i%j === 0) answer.add(i);
+        }
+    }
+    return answer.size;
+}
