1efb4b2c6db1856e3cbfdeb7be0b6f81e1d34d6e
HYEMBURGERWORLD <123081489+HYEMBURGERWORLD@users.noreply.github.com>
2023-03-04 20:59:14 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 1;
+    for(let i = 1; i <= 10; i++) {
+        answer *= i;
+        if(answer === n) {
+            return answer = i;
+        } else if (answer > n) {
+            return i-1;
+        }
+    }
+}
