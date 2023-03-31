bf63f9b28506fd92686d4e44e057eb14ba80b3aa
HYEMBURGERWORLD <123081489+HYEMBURGERWORLD@users.noreply.github.com>
2023-03-31 21:55:33 +0900

    [level 2] Title: 숫자의 표현, Time: 1.59 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let count = 0;
+    
+    for(let i = 1; i <= n; i++) {
+        let num = 0;
+        for(let j = i; j <= n; j++) {
+            num += j;
+            if (num === n) {
+                count++;
+                break;
+            } else if (num > n) {
+                break;
+            }
+        }
+    }
+    
+    return count;
+}
